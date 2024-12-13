e4fa7d6ff937cb34c05a5b25aa347afcce8c2963
kumar327 <adigoppulakumar@gmail.com>
2024-12-13 15:51:27 +0530

    git ignore file added

--- src/com/aks/creational/SingletonDateUtil.java
+++ src/com/aks/creational/SingletonDateUtil.java
@@ -5,7 +5,11 @@ public class SingletonDateUtil {
 	public SingletonDateUtil singletonDateUtil;
 
 	private SingletonDateUtil() {
+		System.out.println("Hello");
+	}
 
+	public static void main(String[] args) {
+		System.out.println("Test");
 	}
 
 }
